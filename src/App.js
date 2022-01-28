820e45f35adc1ad7c71eb5bea53dfb730052145d
Irene-ArkonIT <irene.sabrina@arkonit.se>
2022-01-28 09:08:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-//import logo from './logo.svg';
 import './App.css';
-import React, { Component, useRef } from 'react';
-import axios from 'axios';
+import React, { Component, Fragment} from "react";
+import axios from "axios";
+import Select from "react-select";
 
 class App extends Component {
   constructor(props) {
@@ -10,211 +10,195 @@ class App extends Component {
       allData: [],
       filteredData1: [],
       filteredData2: [],
-      selectedSearch: []
-    }
-    this.myRef = React.createRef();
+      selectedSearch: [],
+      sValue1: [],
+      sValue2: [],
+      sValue3: []      
+    };
   }
 
-  handleSearch (event) {
-    let value = event.target.value;
-    let result1 = [];
-  
+  /*loadOptions (query) {
+    return fetch(`http://localhost:3000/collabs?q=${query}`)
+    .then((res) => res.json());
+  };*/
+
+  handleSearch(event) {
+    let value = event.author;
+    let result = [];
+
+     this.setState({ sValue1: event }, () => 
+      console.log(this.state.sValue1)
+    );
+
     console.log(value);
-    if(value==="select name"){
-      
-      this.setState ({filteredData1: []})
-      this.setState ({filteredData2: []})
-      this.setState ({selectedSearch: []})
-    }
-    
-    result1 = this.state.allData.filter((data) => {
-      return data.author.search(value) !== -1 
+    result = this.state.allData.filter((data) => {
+      return data.author.search(value) !== -1;
     });
 
-    this.setState({filteredData1: result1}, () => 
-            console.log(this.state.filteredData1)
-    )
+    this.setState({ filteredData1: result }, () =>
+      console.log(this.state.filteredData1)
+    );
 
-    this.setState ({filteredData2: []})
+    this.setState({ filteredData2: [] });
+    this.setState({ selectedSearch: result });
+
+    this.setState({ sValue2: null })
+    this.setState({ sValue3: null })
   }
 
-  setID (event) {
-    let value = event.target.value;
-    let result2 = [];
+  setID(event) {
+    let value = event.id;
+    let result = [];   
+    this.setState({sValue2: event}, () => 
+      console.log(this.state.sValue2)
+    );
 
-    result2 = this.state.filteredData1.filter((data) => {
-      return (data.author.search(value) && data.id.search(value) !== -1)
-    })
-    console.log(result2)
-    this.setState({filteredData2: result2}, () =>
-      console.log(this.state.filteredData2)    
-    )
+    result = this.state.filteredData1.filter((data) => {
+      return data.author.search(value) && data.id.search(value) !== -1;
+    });
+    console.log(result);
+    this.setState({ filteredData2: result }, () =>
+      console.log(this.state.filteredData2)
+    );
+    this.setState({ selectedSearch: result });
   }
 
   setURL (event) {
-    let value = event.target.value;
-    console.log(value);   
-  } 
+    // let value = event.target.value;
+    // console.log(value)
 
-  resetValue () {
-    this.setState ({filteredData1: []})
-    this.setState ({filteredData2: []})
-    this.setState ({selectedSearch: []})
-    document.getElementById("authorname").selectedIndex = 0; //1 = option 2  
+    // this.setState({selectedSearch: this.state.filteredData2}, () => 
+    // console.log(this.state.selectedSearch)
+    // )
+
+    this.setState({sValue3: event}, () => 
+      console.log(this.state.sValue2)
+    );
   }
 
-  searchData (data) {
-    this.setState({selectedSearch: this.state.filteredData2}, () =>
-      console.log(this.state.selectedSearch)
-    )
+  resetValue() {
+    this.setState({ filteredData1: [] });
+    this.setState({ filteredData2: [] });
+    this.setState({ selectedSearch: [] });
+    this.setState({ sValue1: [] });
+    this.setState({ sValue2: [] });
+    this.setState({ sValue3: [] });
 
+  }
 
-    let printElement = document.getElementById('#sresult');
-    var printWindow = window.open('', 'PRINT');
-    printWindow.document.write(document.documentElement.innerHTML);
-    setTimeout(() => { // Needed for large documents
-      printWindow.document.body.style.margin = '0 0';
-      printWindow.document.body.innerHTML = printElement.outerHTML;
-      printWindow.document.close(); // necessary for IE >= 10
-      printWindow.focus(); // necessary for IE >= 10*/
-      printWindow.print();
-      printWindow.close();
-    }, 1000)
-
- /*     // Getting user input
-     var message = ("input[name=message]").val();
-  
-     // Opening URL
-     window.open(
-         "whatsapp://send?text=" + message,
-
-         // This is what makes it 
-         // open in a new window.
-         '_blank' 
-     ); */
- 
-    /* let val1 = this.state.filteredData2.map(obj => {
-      return obj.author + "--" + obj.id +"--" + obj.download_url;
-    })
-    console.log(val1) */
+  searchResults() {
+    var divText = document.getElementById("searchContent").innerHTML;
+    var myWindow = window.open("", "", "width=400,height=300");
+    var doc = myWindow.document;
+    doc.open();
+    doc.write(divText);
+    doc.close();
   }
 
   componentDidMount() {
-    axios.get('https://picsum.photos/v2/list?limit=2000')
-    .then(res => {
-    console.log(res.data)
+    axios
+      .get("https://picsum.photos/v2/list?limit=2000")
+      .then((res) => {
+        console.log(res.data);
 
-    this.setState({
-      allData: res.data,
-      filteredData1: [],
-      filteredData2: [],
-      selectedSearch: []
-    });
-    })
-    .catch(error => {
-      console.log('Error getting data: ' + error);
-    })            
-}
+        this.setState({
+          allData: res.data,
+          //filteredData1: [],
+          //filteredData2: [],
+          //selectedSearch: []
+        });
+        document.getElementById("searchContent").style.display = "none";
+      })
+      .catch((error) => {
+        console.log("Error getting data: " + error);
+      });
+  }
 
-  render() {   
-    return (   
-      <div className="App">
-      <div className="row">
-
-      <div className="column" >    
+  render() {
+    return (
+      <div className="float-container">
         <div className="float-child-left">
-          <h4>Search Author</h4>
+          <label>Search Author</label>
+          <br></br>
+          <br></br>
+          <br></br>
           <hr />
-                
-          <label htmlFor="authorname">Choose Name:</label>
+          <label htmlFor="authorname">Name</label>
           <br></br>
-          <select id='authorname' onChange={(event) => this.handleSearch(event)} >
-              <option>select name</option>
-              {this.state.allData.map((value, index) => { 
-                return(
-                  <option key={value.id}>
-                    {value.author}
-                  </option>
-                )
-              })}
-          </select>
+          <br></br>      
+          <Select
+            id="authorname"
+            value={this.state.sValue1}
+            options={this.state.allData}
+            getOptionLabel={(option) => option.author}
+            getOptionValue={(option) => option.id}
+            onChange={(event) => this.handleSearch(event)}
+            ref={ref => { this.select = ref; }}
+            isSearchable  
+          />
+  
           <br></br>
-          <br></br><br></br>
-          <label htmlFor="authorid">Choose ID:</label>
           <br></br>
-          <select id='authorid' onChange={(event) => this.setID(event)}>
-            <option>select id</option>
-            {this.state.filteredData1.map((value,index)=>{
-              return(        
-                <option key={value.id}>
-                  {value.id}
-                </option>        
-              )
-            })}
-          </select>
-          <br></br><br></br><br></br>
-          <label htmlFor="authorurl">Choose URL:</label>
+          <label htmlFor="authorid">ID</label>
           <br></br>
-          <select id='authorurl' onChange={(event) => this.setURL(event)}>
-            <option>select url</option>
-            {this.state.filteredData2.map((value,index)=>{
-              return(        
-                <option key={value.id}>
-                  {value.url}
-                </option>        
-              )
-            })}
-          </select>
+          <br></br>
+          <Select
+            id="authorid"
+            value={this.state.sValue2}
+            setOptionLabel={(option) => option.id}
+            options={this.state.filteredData1}
+            getOptionLabel={(option) => option.id}
+            getOptionValue={(option) => option.id}
+            onChange={(event) => this.setID(event)}
+            ref={ref => { this.select = ref; }}
+            isSearchable
+          />
+
 
           <br></br>
+          <br></br>
+          <label htmlFor="authorurl">URL</label>
+          <br></br>
+          <br></br>
+          <Select
+            id="authorimageurl"
+            value={this.state.sValue3}
+            setOptionLabel={(option) => option.id}
+            options={this.state.filteredData2}
+            getOptionLabel={(option) => option.url}
+            getOptionValue={(option) => option.id}
+            onChange={(event) => this.setURL(event)}
+            ref={ref => { this.select = ref; }}
+            isSearchable
+          />
+
           <br></br>
           <br></br>
           <button onClick={() => this.resetValue()}>Reset</button>
-          <hr />
-          {this.state.selectedSearch.map((value,index)=>{
-            return(              
-                <div id='sresult' key={value.id}>
-                  Author Name:  {value.author}<br></br>
-                  Image width:  {value.width}<br></br>
-                  Image Height: {value.height}<br></br>
-                  Image URL:    {value.download_url}
-                </div>             
-            )
-          })}    
-          <button onClick={() => this.searchData(document.getElementById(#sresult))}>Search</button>
-        </div>
+          <button id="searchbtn" onClick={() => this.searchResults()}>
+            Search
+          </button>
         </div>
 
-        <div className="column">
-          <div className="float-child-right">
-          <h4>Search Results</h4>      
-          
-          {this.state.filteredData1.map((value,index)=>{
-            return(            
-                <div key={value.id}>
-                  {value.author}------{value.id}
-                </div>              
-            )
+        <div id="searchContent">
+          {this.state.selectedSearch.map((value) => {
+            <br></br>;
+            return (
+              <div>
+                Author Name: {value.author}
+                <br></br>
+                ID: {value.id}
+                <br></br>
+                Image Height: {value.height}
+                <br></br>
+                Image Download Link: {value.download_url} <br></br>
+                <br></br>
+              </div>
+            );
           })}
-          <hr />
-          {this.state.selectedSearch.map((value,index)=>{
-            return(              
-                <div id='sresult' key={value.id}>
-                  Author Name:  {value.author}<br></br>
-                  Image width:  {value.width}<br></br>
-                  Image Height: {value.height}<br></br>
-                  Image URL:    {value.download_url}
-                </div>             
-            )
-          })}    
-          
-          </div>      
         </div>
-        <div className="column" />
       </div>
-      </div>
-      
     );
-  }  
+  }
 }
 export default App;
