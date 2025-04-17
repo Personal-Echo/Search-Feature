c9a651f407ed2fa0eafa66032ebee4d6ed48a775
Irene Ryan <irenesryan@gmail.com>
2025-04-17 18:15:23 +0200

    Fix bugs in the code and redesign the app

--- src/App.js
+++ src/App.js
@@ -1,212 +1,168 @@
-//import logo from './logo.svg';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import { Stack } from '@fluentui/react/lib/Stack';
-//import { Dropdown, DefaultButton, ComboBox, IComboBoxStyles } from '@fluentui/react';
-import { Dropdown } from '@fluentui/react-northstar';
-
-
+import { Dropdown, PrimaryButton } from '@fluentui/react';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-      this.state = {
-        data: [],
-        filteredData: [],
-        filteredDataStates: {},
-        resetValues: false,
-        
-        config: {
-          filters: [
-            {
-              type: "dropdown",
-              id: "authorList",
-              placeholder: "Select Author Name",
-              label: "Dropdown Search 1 ",
-              dataReference: "author",
-              value: "author",
-              required: true,
-              allowFreeform: true,
-              autoComplete: true
-            },
-            {
-              type: "dropdown",
-              id: "authorID",
-              placeholder: "Select ID",
-              label: "Dropdown Search 2",
-              dataReference: "id",
-              value: "id",
-              allowFreeform: true,
-              autoComplete: true
-            },
-            {
-              type: "dropdown",
-              id: "authorURL",
-              placeholder: "Select URL",
-              label: "Dropdown Search 3",
-              dataReference: "url",
-              value: "url",
-              allowFreeform: true,
-              autoComplete: true
-            }
-          ]
-        }
-      };  
-      this.resetState = this.resetState.bind(this);        
-    }
-    
-    handleSearch (event, selectedOption, dataReference, filterId) {    
-
-      let value = selectedOption.text;
-      let result = [];
-      let currentFilterState = {}
-
-      console.log(event);
-      console.log(selectedOption);
-      console.log(dataReference);
-      console.log(filterId);
-      
-      result = this.state.filteredData.filter((data) =>
-      {
-        console.log(data[dataReference].search(value))
-        return data[dataReference].search(value) !== -1 ? true : false;        
-      });
-      console.log(result)
-        //let currentFilterState = {}
-        currentFilterState[filterId] = this.state.filteredData
-        this.setState({
-          config: {
-            filters: this.state.config.filters.map( filter => {
-              if(filter.id == filterId) {
-                return { ...filter, data: this.state.filteredData }
-              } else {
-                return filter
-              }
-            })
-          }
-        })
-
-        this.setState({
-          filteredDataStates: {
-            ...this.state.filteredDataStates,
-            ...currentFilterState
-          },
-          filteredData: result
-        }, () => {
-          console.log(this.state.filteredData)
-          console.log(this.state.filteredDataStates)
-        });   
+    this.initialState = {
+      selectedFilters: {
+        authorList: null,
+        authorID: null,
+        authorURL: null
       }
+    };
 
-      resetState = (event, selectedOption, filterId) => {
+    this.state = {
+      data: [],
+      filteredData: [],
+      selectedFilters: {...this.initialState.selectedFilters},
+      config: {
+        filters: [
+          {
+            type: "dropdown",
+            id: "authorList",
+            placeholder: "Select Author Name",
+            label: "Dropdown Search 1",
+            dataReference: "author",
+            value: "author",
+            required: true
+          },
+          {
+            type: "dropdown",
+            id: "authorID",
+            placeholder: "Select ID",
+            label: "Dropdown Search 2",
+            dataReference: "id",
+            value: "id"
+          },
+          {
+            type: "dropdown",
+            id: "authorURL",
+            placeholder: "Select URL",
+            label: "Dropdown Search 3",
+            dataReference: "url",
+            value: "url"
+          }
+        ]
+      },
+      // Add a key to force re-render of dropdowns
+      resetKey: 0
+    };
 
-      this.setState({resetValues: true})
+    this.handleSearch = this.handleSearch.bind(this);
+    this.resetState = this.resetState.bind(this);
+  }
 
-      this.setState({        
-        filteredData: this.state.data,
-        filteredDataStates: {origin: this.state.data}
-      });
-      this.currentFilterState = null
-/* 
-      this.setState({filteredData: this.state.data}, () => 
-            console.log(this.state.filteredData)
-      )
-
-      this.setState({filteredDataStates: this.state.data}, () => 
-          console.log(this.state.filteredDataStates)
-      )  */
-
-      };  
-    
   componentDidMount() {
-      axios.get('https://picsum.photos/v2/list?limit=2000')
+    axios.get('https://picsum.photos/v2/list?limit=2000')
       .then(res => {
-      console.log(res.data)
-
-      this.setState({
-        data: res.data,
-        filteredData: res.data,
-        filteredDataStates: {
-          origin: res.data
-        }
-      });
-          
-      //this.setState({filteredData: res.data});
+        this.setState({
+          data: res.data,
+          filteredData: res.data
+        });
       })
       .catch(error => {
-        console.log('Error getting data: ' + error);
-      })            
+        console.error('Error getting data: ', error);
+      });
   }
-    render () {   
-      //console.log("render() method");
-      const dropdownStylesCustom = {
-        dropdown: { width: 300 }, 
-        root: { maxWidth: 300},
-      };      
-      const dropdownStyles = {
-   
-        ...dropdownStylesCustom
-      }
-    
-      
-      const getOptions = (dataReference, value) => {
-        if(value) {
-          return value.map(obj => {
-            return { key: obj[dataReference].replaceAll(" ", "_"), text: obj[dataReference] }
-          })
-        } else {
-          return this.state.filteredData.map(obj => {
-            return { key: obj[dataReference].replaceAll(" ", "_"), text: obj[dataReference] }
-          })
-        }
-      }
 
-      const stackTokens = { childrenGap: 20 };   
-            
-       return (
-       <Stack tokens={stackTokens}>
-         
-              {
-                this.state.config.filters.map(filter => {
-                    return <Dropdown
-                      search
-                      id={filter.id}
-                      placeholder={filter.placeholder}
-                      label={filter.label}
-                      items={getOptions(filter.dataReference, filter.data ? filter.data : undefined)}
-                      styles={dropdownStyles}
-                      onChange = {(event, selectedOption) => this.handleSearch(event, selectedOption, filter.dataReference, filter.id)}
-                      noResultsMessage="We couldn't find any matches."
-                      getA11ySelectionMessage={{
-                        onAdd: item => `${item} has been selected.`,  }}
-                    />                   
-                })                
-              } 
-         
-              {/* {
-                this.state.config.filters.map(filter => {
-                    return <ComboBox
-                      //className='select'
-                      allowFreeform= {filter.allowFreeform}
-                      autoComplete= {filter.autoComplete ? "on" : "off"}
-                      id={filter.id}
-                      placeholder={filter.placeholder}
-                      label={filter.label}
-                      options={getOptions(filter.dataReference, filter.data ? filter.data : undefined)}
-                      styles={dropdownStyles}
-                      onChange = {(event, selectedOption) => this.handleSearch(event, selectedOption, filter.dataReference, filter.id)}
-                      //selectedKey={ getOptions }
-                      //notifyOnReselect={true}                     
-                    />                   
-                })                
-              }      */}
+  handleSearch(event, option, dataReference, filterId) {
+    const { selectedFilters, config } = this.state;
+    const newSelectedFilters = {
+      ...selectedFilters,
+      [filterId]: option?.text || ''
+    };
 
-               
-        
-        </Stack>
-      );
-  }  
+    // Filter the data according to the selected dropdowns up to this point
+    let newFilteredData = this.state.data;
+    config.filters.forEach((filter) => {
+      const selected = newSelectedFilters[filter.id];
+      if (selected) {
+        newFilteredData = newFilteredData.filter(item =>
+          String(item[filter.dataReference] || '').toLowerCase() === selected.toLowerCase()
+        );
+      }
+    });
+
+    this.setState({
+      selectedFilters: newSelectedFilters,
+      filteredData: newFilteredData
+    });
+  }
+
+  resetState() {
+    // Reset the selected filters to null and restore the filtered data to original
+    this.setState(prevState => ({
+      selectedFilters: {...this.initialState.selectedFilters},
+      filteredData: this.state.data,
+      // Increment reset key to force re-render of dropdowns
+      resetKey: prevState.resetKey + 1
+    }));
+  }
+
+  render() {
+    const { filteredData, config, selectedFilters, resetKey } = this.state;
+
+    const dropdownStyles = { dropdown: { width: '100%' }, root: { width: '100%' } };
+    const stackTokens = { childrenGap: 15 };
+
+    const getOptions = (dataReference) => {
+      const source = filteredData; // Use filtered data for subsequent dropdowns
+      const uniqueValues = new Set();
+      return source
+        .filter(item => !!item[dataReference])
+        .filter(item => {
+          if (!uniqueValues.has(item[dataReference])) {
+            uniqueValues.add(item[dataReference]);
+            return true;
+          }
+          return false;
+        })
+        .map(obj => {
+          const text = String(obj[dataReference] || '');
+          return {
+            key: text.replace(/\s+/g, "_"),
+            text
+          };
+        });
+    };
+
+    const isDropdownDisabled = (index) => {
+      if (index === 0) return false; // First dropdown is always enabled
+      const prevFilter = config.filters[index - 1];
+      return !selectedFilters[prevFilter.id]; // Disable if previous dropdown has no selection
+    };
+
+    return (
+      <div className="App">
+        <div className="card">
+          <h1>Search Authors</h1>
+          <Stack tokens={stackTokens}>
+            {config.filters.map((filter, index) => (
+              <Dropdown
+                key={`${filter.id}-${resetKey}`} // Add resetKey to force re-render on reset
+                placeholder={filter.placeholder}
+                label={filter.label}
+                options={getOptions(filter.dataReference)}
+                styles={dropdownStyles}
+                onChange={(event, option) =>
+                  this.handleSearch(event, option, filter.dataReference, filter.id)
+                }
+                disabled={isDropdownDisabled(index)}
+                selectedKey={undefined} // Always start with undefined, let controlled component handle selection
+                defaultSelectedKey={undefined} // Ensure no default selection
+              />
+            ))}
+            <PrimaryButton text="Reset" onClick={this.resetState} style={{ marginTop: 20 }} />
+          </Stack>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default App;
